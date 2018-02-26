665e2ff1a606fbc515897ac1bb3f54e74d080569
speedyplane2247 <36555661+speedyplane2247@users.noreply.github.com>
2018-02-26 10:02:06 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@ jsCoder
 (c) speedyplane 2247
 Client Backend, under MIT license.
 */
+function encode() {
 var decodedURI = input; // input comes 
 var encodedURI = encodeURI(decodedURI);
+}
+function decode() {
+  // tbd
+}
+
 /*
 Continue
 */
